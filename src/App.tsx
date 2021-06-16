3ce94cd53df5bcf640927a67cef130ede0ff5f1f
hafizalfaza <hafizalfaza@gmail.com>
2021-06-16 09:44:48 +0700

    feat: render SongsListPage in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
+import './App.scss';
+import SongsListPage from './pages/SongsListPage/SongsListPage'
+import Switch from "react-switch"
+import dayIcon from './assets/icons/day.png'
+import nightIcon from './assets/icons/night.png'
 
 function App() {
+
+  const isInitialThemeDark = localStorage.getItem('is_dark_theme')
+
+  const [isDarkMode, setIsDarkMode] = useState(!!isInitialThemeDark)
+  const appRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    toggleDarkMode(isDarkMode)
+    localStorage.setItem('is_dark_theme', isDarkMode ? 'true' : '')
+  }, [isDarkMode])
+
+  const toggleDarkMode = (isDarkMode: boolean) => {
+    if (isDarkMode) {
+      appRef?.current?.classList.add('dark')
+    } else {
+      appRef?.current?.classList.remove('dark')
+    }
+  }
+
+  const renderNightIcon = () => {
+    return (
+      <div style={{ display: 'flex', height: '100%', alignItems: 'center', justifyContent: 'center' }}>
+        <img src={dayIcon} alt='dark-mode-icon' style={{ width: 20 }}/>
+      </div>
+    )
+  }
+
+  const renderDayIcon = () => {
+    return (
+      <div style={{ display: 'flex', height: '100%', alignItems: 'center', justifyContent: 'center' }}>
+        <img src={nightIcon} alt='light-mode-icon' style={{ width: 20 }}/>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' ref={appRef}>
+      <SongsListPage />
+      <div className='theme-switch-container'>
+        <Switch onChange={() => setIsDarkMode(!isDarkMode)} checked={isDarkMode} checkedIcon={renderDayIcon()} uncheckedIcon={renderNightIcon()} offColor='#18191A' onColor='#dddddd'/>
+      </div>
+      
     </div>
   );
 }
